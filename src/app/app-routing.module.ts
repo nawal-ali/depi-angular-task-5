c4a08a8f871645216de4480e22d6e33e6394ebb2
nawal-ali <nmaaa3899@gmail.com>
2025-04-08 00:06:21 +0200

    done nav with routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { CategoriesComponent } from './pages/categories/categories.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:HomeComponent},
+  {path:'categories',component:CategoriesComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
